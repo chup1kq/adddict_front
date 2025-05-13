42960a2ba89010cec8fc800180c702069ccf30ee
Zinovev Ivan <izinovev77@gmail.com>
2025-05-13 10:11:55 +0300

    picture changed

--- src/components/QuizResult.jsx
+++ src/components/QuizResult.jsx
@@ -1,4 +1,4 @@
-import trophy from '../static/images/trophy.png';
+import nice from '../static/images/white_check_mark.png';
 import thumbsUp from '../static/images/+1.png';
 import slightlyFrowning from '../static/images/slightly_frowning_face.png';
 import sweat from '../static/images/sweat.png';
@@ -7,12 +7,11 @@ export const Result = ({ count, correctCount }) => {
     const percentage = Math.round((correctCount / count) * 100);
 
     const getResultIcon = () => {
-        if (percentage >= 90) return trophy;
+        if (percentage >= 90) return nice;
         if (percentage >= 60) return thumbsUp;
         if (percentage >= 30) return slightlyFrowning;
         return sweat;
     };
-
 
     const getResultText = () => {
         if (percentage >= 90) {
@@ -40,7 +39,6 @@ export const Result = ({ count, correctCount }) => {
                 <button className="primary-button">
                     Попробовать снова
                 </button>
-
                 {correctCount < count && (
                     <button className="secondary-button">
                         Работа над ошибками
