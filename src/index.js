ae3cc834a12f0a3758482a77de45fddd2c4245d4
Zinovev Ivan <izinovev77@gmail.com>
2025-05-24 01:53:28 +0300

    StrictMode off

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    //<React.StrictMode>
         <AuthProvider>
             <App/>
         </AuthProvider>
-    </React.StrictMode>
+    //</React.StrictMode>
 );
