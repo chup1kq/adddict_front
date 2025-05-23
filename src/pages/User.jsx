1416aef69be3d89e624ab96ae40ce6f4f85bbf47
Zinovev Ivan <izinovev77@gmail.com>
2025-05-23 23:43:19 +0300

    fixed: create dict

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -82,9 +82,6 @@ export const User = () => {
             console.error("Ошибка при создании словаря:", error);
         }
 
-        console.log('Создание нового словаря:', dictToAdd);
-        // Здесь должна быть логика создания словаря через API
-        console.log(dictionaryApi.createDictionary(dictToAdd, localStorage.getItem("token")));
         setShowAddDictionaryModal(false);
     };
 
